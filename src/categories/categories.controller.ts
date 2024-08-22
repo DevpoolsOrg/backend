f30dfe4a3a52433dc8d2e11d2b6aad05120931b9
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-08-22 15:33:11 -0300

    chore: Update CategoriesController with authentication decorators and roles

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -3,35 +3,39 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces';
 
 
 @Auth()
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) {};
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.SUPERADMIN)
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
-  }
+  };
 
   @Get()
   async findAll() {
     return this.categoriesService.findAll();
-  }
+  };
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(id);
-  }
+  };
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.SUPERADMIN)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoriesService.update(id, updateCategoryDto);
-  }
+  };
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.SUPERADMIN)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return this.categoriesService.remove(id);
-  }
-}
+  };
+};
