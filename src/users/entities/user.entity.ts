199a96f2533f604b3de36c736ce957b6179a9078
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-08-23 11:50:52 -0300

    chore: Add Vote entity and update User entity with votes relationship

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,7 @@
 import { Exclude } from "class-transformer";
 import { ValidRoles } from "src/auth/interfaces";
 import { Post } from "src/posts/entities/post.entity";
+import { Vote } from "src/votes/entities/vote.entity";
 import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 
@@ -8,48 +9,50 @@ import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, OneToMany
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
-    
+
     @Column('text')
     name: string;
 
-    @Column('text', {unique: true})
+    @Column('text', { unique: true })
     email: string;
 
     @Column('text')
-    @Exclude({'toPlainOnly': true})
+    @Exclude({ 'toPlainOnly': true })
     password: string;
 
-    @Column('text',{ 
+    @Column('text', {
         array: true,
         default: [ValidRoles.USER],
     })
     roles: string[];
 
-    @Column('text',{nullable: true})
+    @Column('text', { nullable: true })
     avatar?: string;
 
-    @Column('bool', {default: true})
+    @Column('bool', { default: true })
     isActive: boolean;
 
     @OneToMany(() => Post, post => post.user)
     posts: Post[];
-    
+    @OneToMany(() => Vote, vote => vote.user)
+    votes: Vote[];
+
 
     @CreateDateColumn()
-    @Exclude({'toPlainOnly': true})
+    @Exclude({ 'toPlainOnly': true })
     createdAt: Date;
-    
+
     @CreateDateColumn()
-    @Exclude({'toPlainOnly': true})
+    @Exclude({ 'toPlainOnly': true })
     updatedAt: Date;
 
     @BeforeInsert()
-    checkFieldsBeforeInsert(){
+    checkFieldsBeforeInsert() {
         this.email = this.email.toLowerCase().trim();
     };
-    
+
     @BeforeUpdate()
-    checkFieldsBeforeUpdate(){
+    checkFieldsBeforeUpdate() {
         this.checkFieldsBeforeInsert();
     };
 
